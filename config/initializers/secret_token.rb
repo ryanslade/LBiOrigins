8bd465105207a10a031c4e45a4fb4a85485e13cb
Ryan Slade <ryan.slade@lbi.com>
2011-04-14 17:58:14 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LBiOrigins::Application.config.secret_token = '1b9b5adfd8e0709c1744bdc72f5500ab1e133fd33155c5cc6423687c749888376d68c2011d5b3621d21313da5a556edf9b247df7bd8147273168bd2b49eb769f'
