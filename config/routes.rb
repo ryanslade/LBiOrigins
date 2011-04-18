d4c1c2b33435c28f575fe2b710ed37ce3da98c5d
Ryan Slade <ryan.slade@lbi.com>
2011-04-18 17:08:00 +0100

    Added map route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ LBiOrigins::Application.routes.draw do
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+  match 'map' => 'map#index'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
