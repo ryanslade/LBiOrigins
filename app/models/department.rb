34bec42c41246b9ed90b5015d551c920a7d6e8be
Ryan Slade <ryan.slade@lbi.com>
2011-04-19 13:00:52 +0100

    Department dropdown now has a "Please select" option

--- app/models/department.rb
+++ app/models/department.rb
@@ -4,8 +4,8 @@ class Department < ActiveRecord::Base
   
   validates_presence_of :name
   
-  def self.dropdown_options
-    self.order(:name).select("name, id").collect { |d| [d.name, d.id] }
+  def self.dropdown_options(default_text = "Please Select")
+    [[default_text, nil]] + self.order(:name).select("name, id").collect { |d| [d.name, d.id] }
   end
   
 end
