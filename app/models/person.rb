662f31edad2ce3cff4af577a7c644a11f003ac4e
Ryan Slade <ryan.slade@lbi.com>
2011-04-20 15:58:33 +0100

    Randomise lat and long by a small amount so that pins don't cover each other

--- app/models/person.rb
+++ app/models/person.rb
@@ -15,7 +15,19 @@ class Person < ActiveRecord::Base
   def geocode
     address = "#{home_town}, #{home_country}"
     result = GoogleMaps.geocode(address)
-    self.latitude, self.longitude = result[:lat], result[:lng] if result
+    
+    if result
+      self.latitude, self.longitude = result[:lat], result[:lng]
+      
+      # If another user has the same location, randomise by a small amount
+      # so their pin's dont cover each other
+      if (Person.where(:latitude => self.latitude, :longitude => self.longitude).count > 0)
+        self.latitude  += (rand(150)-100).to_f/10000
+        self.longitude += (rand(150)-100).to_f/10000
+      end
+
+    end
+    
   end
   
 end
